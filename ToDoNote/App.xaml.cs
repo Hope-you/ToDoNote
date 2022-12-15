0f9bfb815d1a10dcf0dade211e33768d9a457381
hope-you <595318443@qq.com>
2022-12-15 17:09:44 +0800

    添加项目文件。

---
+++ ToDoNote/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using DryIoc;
+using Prism.DryIoc;
+using Prism.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ToDoNote
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+
+           return this.Container.Resolve<MainWindow>();
+        }
+
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
