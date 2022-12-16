fa52233dcae7f5cde43ae50f3d1f6d96c1e03962
hope-you <595318443@qq.com>
2022-12-16 15:33:10 +0800

    indexView首页设计1/2

---
+++ ToDoNote/Common/Models/TaskBar.cs
@@ -0,0 +1,57 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoNote.Common.Models
+{
+    public class TaskBar:BindableBase
+    {
+        private string icon;
+        private string title;
+        private string content;
+        private string color;
+        private string target;
+
+
+        public string Icon
+        {
+            get { return icon; }
+            set { icon = value; }
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string Content
+        {
+            get { return content; }
+            set { content = value; }
+        }
+
+        /// <summary>
+        /// 背景颜色
+        /// </summary>
+        public string Color
+        {
+            get { return color; }
+            set { color = value; }
+        }
+
+        /// <summary>
+        /// 触发的目标模块
+        /// </summary>
+        public string Target
+        {
+            get { return target; }
+            set { target = value; }
+        }
+
+
+    }
+}
--- ToDoNote/ViewModels/IndexViewModel.cs
+++ ToDoNote/ViewModels/IndexViewModel.cs
@@ -1,12 +1,53 @@
-﻿using System;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using ToDoNote.Common.Models;
 
 namespace ToDoNote.ViewModels
 {
-    public class IndexViewModel
+    public class IndexViewModel : BindableBase
     {
+        private string welecomTitle;
+
+        public string WelecomTitle
+        {
+            get { openSaying(); return welecomTitle; }
+            set { welecomTitle = value; }
+        }
+
+        public IndexViewModel()
+        {
+            TaskBarsList = new ObservableCollection<TaskBar>();
+            CreateTaskBar();
+        }
+
+        private async void openSaying()
+        {
+            var client = new HttpClient();
+            var res = client.Send(new HttpRequestMessage() { RequestUri = new Uri("https://v1.jinrishici.com/rensheng.txt") });
+            WelecomTitle = await res.Content.ReadAsStringAsync();
+        }
+
+        private ObservableCollection<TaskBar> taskBarList;
+
+        public ObservableCollection<TaskBar> TaskBarsList
+        {
+            get { return taskBarList; }
+            set { taskBarList = value; }
+        }
+        void CreateTaskBar()
+        {
+            TaskBarsList.Add(new TaskBar { Icon = "Alarm", Title = "汇总", Color = "#0097ff", Content = "9", Target = "" });
+            TaskBarsList.Add(new TaskBar { Icon = "Check", Title = "已完成", Color = "#10b138", Content = "10", Target = "" });
+            TaskBarsList.Add(new TaskBar { Icon = "SineWave", Title = "完成率", Color = "#00b4df", Content = "11", Target = "" });
+            TaskBarsList.Add(new TaskBar { Icon = "NotebookHeart", Title = "备忘录", Color = "#ffa000", Content = "100%", Target = "" });
+        }
+
     }
 }
