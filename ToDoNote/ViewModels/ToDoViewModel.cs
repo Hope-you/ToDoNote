8a06d47cad594d9045c84189f69150290db43c5a
hope-you <595318443@qq.com>
2022-12-19 16:46:33 +0800

    完善了IndexView待办的右侧栏弹不出来

--- ToDoNote/ViewModels/ToDoViewModel.cs
+++ ToDoNote/ViewModels/ToDoViewModel.cs
@@ -1,12 +1,62 @@
-﻿using System;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToDoNote.Common.Models;
 
 namespace ToDoNote.ViewModels
 {
-    public class ToDoViewModel
+    public class ToDoViewModel : BindableBase
     {
+        /// <summary>
+        /// 单击右侧添加待办展示侧边栏
+        /// </summary>
+        private bool isLeftDrawerOpen;
+
+        public bool IsLeftDrawerOpen
+        {
+            get { return isLeftDrawerOpen; }
+            set { isLeftDrawerOpen = value; RaisePropertyChanged(); }
+        }
+
+        public ToDoViewModel()
+        {
+            ToDoDtos = new ObservableCollection<ToDoDto>();
+            AddToDoCommand = new DelegateCommand(AddToDo);
+            CreateToDoListData();
+        }
+
+        private void AddToDo()
+        {
+            IsLeftDrawerOpen = !IsLeftDrawerOpen;
+        }
+
+        private ObservableCollection<ToDoDto> toDoDtos;
+
+        public DelegateCommand AddToDoCommand { get; private set; }
+
+        public ObservableCollection<ToDoDto> ToDoDtos
+        {
+            get { return toDoDtos; }
+            set { toDoDtos = value; RaisePropertyChanged(); }
+        }
+        void CreateToDoListData()
+        {
+            for (int i = 0; i < 32; i++)
+            {
+                ToDoDtos.Add(new ToDoDto
+                {
+                    Title = "标题" + i,
+                    Content = i + "内容123123123123",
+                    Id = i,
+                    CreateDate = DateTime.Now,
+                });
+            }
+
+        }
     }
 }
